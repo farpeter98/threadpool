171ca0369b7280f7827cb252b16ec15c6a895c56
farpeter98 <farpeter98@gmail.com>
2022-09-02 16:03:14 +0200

    TaskQueueing only exposes futures

--- includes/ThreadPool.hpp
+++ includes/ThreadPool.hpp
@@ -2,42 +2,54 @@
 #define THREADPOOL_H
 
 #include <future>
-#include <memory>
 #include <list>
+#include <memory>
 
 
 namespace ThreadPool {
 
 class ThreadPool {
+    struct TaskEntryBase {
+        std::function<void ()> task;
+    };
+
+    template<typename ResultType>
+    struct TaskEntry : public TaskEntryBase  {
+        std::promise<ResultType> promise;
+    };
+
 public:
     template<typename TaskType, typename... Args>
     auto QueueTask (TaskType&& task, Args&&... args) {
-        using ResultType = decltype (task (args...));
-        using PromiseType = std::promise<ResultType>;
+        using ResultType    = decltype (task (args...));
+        using PromiseType   = std::promise<ResultType>;
+        using EntryType     = TaskEntry<ResultType>;
 
-        std::shared_ptr<PromiseType> promise = std::make_shared<PromiseType> ();
-        tasks.emplace_back ([task = std::move (task), &args..., promise] () -> void {
+        std::shared_ptr<EntryType> entry = std::make_shared<EntryType> ();
+        auto voidTask = [task = std::move (task), &args..., entry] () -> void {
             if constexpr (std::is_void_v <ResultType>) {
                 task (std::forward <Args> (args)...);
-                promise->set_value ();
+                entry->promise.set_value ();
             }
             else {
-                promise->set_value (task (std::forward <Args> (args)...));
+                entry->promise.set_value (task (std::forward <Args> (args)...));
             }
-        });
-        return promise;
+        };
+        entry->task = std::move (voidTask);
+        tasks.emplace_back (entry);
+        return entry->promise.get_future ();
     }
 
 
     void ExecuteTasks () {
-        for (const std::function<void ()>& task : tasks) {
-            task ();
+        for (const std::shared_ptr<TaskEntryBase>& entry : tasks) {
+            entry->task ();
         }
     }
 
 
 private:
-    std::list<std::function<void ()>> tasks;
+    std::list<std::shared_ptr<TaskEntryBase>> tasks;
 };
 
 } // namespace ThreadPool
--- main.cpp
+++ main.cpp
@@ -13,11 +13,9 @@ int main ()
     auto voidTask = [&] (int y) -> void {
         x = y;
     };
-    std::shared_ptr<std::promise<int>> intPromise = pool.QueueTask (std::move (task), 12, 35);
-    std::shared_ptr<std::promise<void>> voidPromise = pool.QueueTask (std::move (voidTask), 3);
+    std::future<int> intResult = pool.QueueTask (std::move (task), 12, 35);
+    std::future<void> voidResult = pool.QueueTask (std::move (voidTask), 3);
     pool.ExecuteTasks ();
-    std::future<int> intResult = intPromise->get_future ();
-    std::future<void> voidResult = voidPromise->get_future ();
     voidResult.wait ();
     std::cout << x << std::endl;
     std::cout << intResult.get () << std::endl;
